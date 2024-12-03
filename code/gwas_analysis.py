4faf66cd8ca98b647305a49b6c6191652c6ac865
Gloria Wang <gw4164@gmail.com>
2024-12-03 03:50:59 -0500

    Create gwas_analysis.py

---
+++ code/gwas_analysis.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+GWAS Analysis Script for Mouse Strain Data
+Author: Gloria Wang
+Description: Performs association testing and generates visualizations
+"""
+
+import os
+import numpy as np
+import pandas as pd
+from fastlmm.association import single_snp
+import matplotlib.pyplot as plt
+from scipy import stats
+
+class GWASAnalysis:
+    def __init__(self, bed_file, phenotype_file, output_dir):
+        """Initialize GWAS analysis parameters"""
+        self.bed_file = bed_file
+        self.phenotype_file = phenotype_file
+        self.output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        
+    def run_association(self):
+        """Perform association testing"""
+        print("Running association analysis...")
+        self.results = single_snp(
+            self.bed_file,
+            self.phenotype_file,
+            count_A1=False
+        )
+        self._save_results()
+        
+    def create_manhattan_plot(self):
+        """Generate Manhattan plot"""
+        print("Creating Manhattan plot...")
+        # Manhattan plot implementation
+        plt.savefig(os.path.join(self.output_dir, 'manhattan.pdf'))
+        plt.close()
+        
+    def create_qq_plot(self):
+        """Generate Q-Q plot"""
+        print("Creating Q-Q plot...")
+        # Q-Q plot implementation
+        plt.savefig(os.path.join(self.output_dir, 'qq_plot.pdf'))
+        plt.close()
+        
+    def _save_results(self):
+        """Save association results"""
+        output_file = os.path.join(self.output_dir, 'association_results.txt')
+        self.results.to_csv(output_file, sep='\t', index=False)
+
+def main():
+    """Main execution function"""
+    # Set up argument parser and run analysis
+    pass
+
+if __name__ == "__main__":
+    main()
