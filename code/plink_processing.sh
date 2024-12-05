65a5beccdae6622d9a5af5b8c983c608b1420463
Gloria Wang <gw4164@gmail.com>
2024-12-05 00:12:36 -0500

    Update plink_processing.sh

--- code/plink_processing.sh
+++ code/plink_processing.sh
@@ -1,31 +1,18 @@
-#!/bin/bash
+# Purpose: Processes merged VCF data through PLINK
 
-# PLINK Processing Script for Mouse Strain GWAS
-# Author: Gloria Wang
-# Description: Processes merged VCF data through PLINK
-
-# Set default parameters
 INPUT_VCF=${1:-"data/processed/merged.vcf.gz"}
 OUTPUT_PREFIX=${2:-"data/processed/plink/filtered_snps"}
 GENO_THRESHOLD=0.1
-
-# Create output directory
 mkdir -p "$(dirname "$OUTPUT_PREFIX")"
 
-# Convert VCF to PLINK binary format
-echo "Converting VCF to PLINK format..."
 plink --vcf "$INPUT_VCF" \
       --make-bed \
       --out "${OUTPUT_PREFIX}_initial"
 
-# Apply quality filters
-echo "Applying quality filters..."
+
 plink --bfile "${OUTPUT_PREFIX}_initial" \
       --geno "$GENO_THRESHOLD" \
       --make-bed \
       --out "$OUTPUT_PREFIX"
 
-# Clean up intermediate files
 rm "${OUTPUT_PREFIX}_initial"*
-
-echo "PLINK processing complete. Output files: ${OUTPUT_PREFIX}.[bed/bim/fam]"
